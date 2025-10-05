a40cbd1eb808de671bb69a29fa1a03bb7ac4c563
liambakk <liambakker2@gmail.com>
2025-10-05 17:04:02 +0100

    Fix React import and add virtual-tryon directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Scene } from '@/components/3d/Scene';
 import { MeasurementForm } from '@/components/ui/MeasurementForm';
 import { MeasurementProvider } from '@/contexts/MeasurementContext';
